3b584e2688d5f6924ff0cf07babdb0f93d006905
nhattmhe182558 <51317797+bruh12345678@users.noreply.github.com>
2023-10-27 21:58:44 +0700

    update

--- Algo_visualized.py
+++ Algo_visualized.py
@@ -7,16 +7,20 @@ data = [[ 1,0,4],
         [-1,1,4],
         [ 0,2,4],
         [ 2,1,3]]
+fig = plt.figure()
 
+ax = fig.add_subplot(111, projection='3d')
+
+data = np.array(data).T
 def pca(data, num_components):
     # Center the data
     centered_data = np.array(data - np.mean(data, axis=0))
-    print(centered_data.shape)
+
     # Compute the covariance matrix
-    covariance_matrix = np.cov(centered_data, rowvar=False)
+    cov_matrix = np.dot(centered_data, centered_data.T) / (len(data) - 1)
 
     # Perform eigendecomposition on the covariance matrix
-    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
 
     # Sort the eigenvalues and eigenvectors in descending order
     sorted_indices = np.argsort(eigenvalues)[::-1]
@@ -26,17 +30,11 @@ def pca(data, num_components):
     selected_eigenvectors = sorted_eigenvectors[:, :num_components]
 
     # Transform the data to the new coordinate system
-    transformed_data = np.dot(centered_data,selected_eigenvectors )
-
+    transformed_data = np.dot(selected_eigenvectors.T,centered_data )
     return transformed_data
 
 transformed_data = pca(data,2)
-fig = plt.figure()
-
-ax = fig.add_subplot(111, projection='3d')
-
-for column in transformed_data:
-    ax.scatter(column[0], column[1], color='red')
-
 print(transformed_data)
+for row in transformed_data.T:
+    ax.scatter(row[0],row[1])
 plt.show()
--- Image_compression.py
+++ Image_compression.py
@@ -1,26 +1,31 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-
 # Read the JPG image using PIL
 image = Image.open('test_img.jpg')
 
 # Convert the image to a NumPy array
 image_array = np.array(image)
 
+REDUCE_TO=50
+
 # Flatten the image array
-flattened_array = image_array.reshape(-1, image_array.shape[-1])
+flattened_array =(image_array.reshape(-1, image_array.shape[-1]) )
 # Perform PCA to reduce dimensionality
-pca = PCA(n_components=1)  # Specify the desired number of components
-reduced_array = pca.fit_transform(flattened_array)
-print(reduced_array)
-reconstructed_array = pca.inverse_transform(reduced_array)
+mean = flattened_array.mean()
 
-# Reshape the reconstructed array to the original shape
-reconstructed_image = reconstructed_array.reshape(image_array.shape)
+standardlize_matrix = np.array(flattened_array - mean)
 
-# Display the restored image
-plt.imshow(reconstructed_image.astype(np.uint8))
-plt.axis('off')  # Turn off the axis labels
-plt.show()
+cov_matrix = np.dot(standardlize_matrix,standardlize_matrix.T) / len(standardlize_matrix)
+
+eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+
+idx = np.argsort(eigenvalues)[::-1]
+
+eigenvectors = eigenvectors[:, idx]
+selected_eigenvectors = eigenvectors[:,:REDUCE_TO]
+
+
+new_coordinates = np.dot(selected_eigenvectors.T, standardlize_matrix)
+
+print(new_coordinates.shape)
